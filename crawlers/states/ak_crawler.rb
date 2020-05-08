1cd126e290ae650a9905f86291ca1af2a0caa704
Robert Buchberger <robert@buchberger.cc>
2020-05-08 11:26:58 +0200

    Improve alaska crawler formatting
    I like to keep rubocop happy, sorry to be nitpicky.

--- crawlers/states/ak_crawler.rb
+++ crawlers/states/ak_crawler.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
+# Crawl Alaska
 class AkCrawler < BaseCrawler
-
   protected
 
   def _set_up_page
-    url = @driver.page_source.scan(/https[^'"]+arcgis\.com\/apps\/opsdashboard[^'"]+/)[0]
+    url = @driver.page_source
+                 .scan(%r{https[^'"]+arcgis\.com/apps/opsdashboard[^'"]+})[0]
     @driver.navigate.to(url)
   end
 
@@ -13,7 +14,7 @@ class AkCrawler < BaseCrawler
     w = /Total Cases(\d+)/.match(_page_elements)
     return unless w
 
-    @results[:positive] = w[1].to_i 
+    @results[:positive] = w[1].to_i
   end
 
   def _find_tested
@@ -29,33 +30,33 @@ class AkCrawler < BaseCrawler
 
     @results[:tested] = string_to_i(value)
   end
-  
+
   def _find_deaths
     w = /Total Deaths(\d+)/.match(_page_elements)
     return unless w
 
-    @results[:deaths] = w[1].to_i 
+    @results[:deaths] = w[1].to_i
   end
 
   def _find_recovered
     w = /Total Recovered Cases(\d+)/.match(_page_elements)
     return unless w
 
-    @results[:recovered] = w[1].to_i 
+    @results[:recovered] = w[1].to_i
   end
 
   def _find_hospitalized
     w = /Total Hospitalizations(\d+)/.match(_page_elements)
     return unless w
 
-    @results[:hospitalized] = w[1].to_i 
+    @results[:hospitalized] = w[1].to_i
   end
 
   private
 
   def _page_elements
-    @_page_elements ||= wait.until {
+    @_page_elements ||= wait.until do
       @driver.find_element(class: 'claro')
-    }.text.tr("\n", '')
+    end.text.tr("\n", '')
   end
 end
