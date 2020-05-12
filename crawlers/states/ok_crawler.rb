dcdcaf250527e7a2a01ea0b8f50842c67f7d03e3
Robert Buchberger <robert@buchberger.cc>
2020-05-12 16:22:35 +0200

    Teach OkParser to find :tested result

--- crawlers/states/ok_crawler.rb
+++ crawlers/states/ok_crawler.rb
@@ -3,10 +3,19 @@
 # Oklahoma
 # https://coronavirus.health.ok.gov/
 class OkCrawler < BaseCrawler
-  protected
+  # We have to override the call method because we need to call _find_tested
+  # last. This is a big red flag; should refactor later.
+  def call
+    _set_up_page
 
-  # Place results in @results[:statistic], i.e. @results[:positive]
-  # Skip if not available/applicable.
+    parse_dashboard
+
+    _find_tested
+    _check_results
+    @results
+  end
+
+  protected
 
   def _set_up_page
     # The iframe is lazy-loaded, meaning we have to scroll it into view:
@@ -14,45 +23,42 @@ class OkCrawler < BaseCrawler
     wait.until { dash_loaded }
 
     @driver.switch_to.frame(dashboard)
-    # The dashboard loads separately, we need to wait until it finishes
-    wait.until { grid_items.any? }
   end
 
-  def _find_positive
-    @results[:positive] = get_number 'OK Cases'
-  end
+  def _find_tested
+    @driver.navigate.to 'https://coronavirus.health.ok.gov/executive-order-reports'
 
-  # _find_tested
-  # if :tested is not available, find :negative
-  # Neither are reported as far as I can tell.
-
-  def _find_deaths
-    @results[:deaths] = get_number 'OK Deaths'
-  end
-
-  def _find_recovered
-    @results[:recovered] = get_number 'OK Recovered'
+    @results[:tested] = latest_report.get_int(
+      /Total Number of Specimens Tested to Date([\d,]+)/
+    )
   end
 
   # _find_hospitalized
-
   # _find_counties
-
   # _find_towns
+
   private
 
-  def get_number(query)
-    string_to_i grid_items.filter { |i| i.text.index(query)&.zero? }
-                          .first.text
-                          .delete_prefix(query).strip
-                          .split.first
+  def parse_dashboard
+    dashboard_query_hash.each_pair do |key, query|
+      @results[key] = get_dashboard_number(query)
+    end
+  end
+
+  # Pair results keys with appropriate get_dashboard_number args
+  def dashboard_query_hash
+    {
+      positive: 'OK Cases',
+      deaths: 'OK Deaths',
+      recovered: 'OK Recovered'
+    }
   end
 
   # Seems to be the cleanest way to pick out data elements
   def grid_items
-    Nokogiri::HTML.parse(@driver.page_source)
-                  .css('div.react-grid-item')
-                  .to_a
+    @grid_items ||= Nokogiri::HTML.parse(@driver.page_source)
+                                  .css('div.react-grid-item')
+                                  .to_a
   end
 
   def dash_loaded
@@ -64,5 +70,23 @@ class OkCrawler < BaseCrawler
   def dashboard
     @driver.find_element xpath:
       "//iframe[contains(@src, 'https://looker-dashboards.ok.gov')]"
+  end
+
+  def latest_report
+    @latest_report ||= PdfParser.new latest_report_uri
+  end
+
+  def latest_report_uri
+    reports = wait.until do
+      @driver.find_elements(xpath: '//a[contains(@type, "application/pdf")]')
+    end
+    reports.first.attribute('href')
+  end
+
+  def get_dashboard_number(query)
+    string_to_i grid_items.filter { |i| i.text.index(query)&.zero? }
+                          .first.text
+                          .delete_prefix(query).strip
+                          .split.first
   end
 end
